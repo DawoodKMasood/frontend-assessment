454aeaa41e75632984ea34a557287772c13b582e
Dawood Khan Masood <dawood.masood@s2smark.com>
2023-06-18 18:51:40 +0500

    Cleanup

--- app/editor/layout.js
+++ app/editor/layout.js
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Frontend Assessment App',
-  description: 'A sample app created for assessment.',
+  title: 'Editor',
+  description: '',
 }
 
 export default function RootLayout({ children }) {
