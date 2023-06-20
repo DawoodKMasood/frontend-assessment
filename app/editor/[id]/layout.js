435eb99e1f893ebddb45c981cc684681b3218012
Dawood Khan Masood <dawood.masood@s2smark.com>
2023-06-20 14:48:15 +0500

    Update layout.js

--- app/editor/[id]/layout.js
+++ app/editor/[id]/layout.js
@@ -7,6 +7,8 @@ export const metadata = {
 
 export default function EditorLayout({ children }) {
   return (
-    <div suppressHydrationWarning={true}>{children}</div>
+    <div suppressHydrationWarning={true}>
+      {children}
+    </div>
   )
 }
